cb609ba260f78938969e19720a74f7d7a4efea14
OwJuice <76753394+OwJuice@users.noreply.github.com>
2024-02-29 01:35:05 -0500

    Created initial HW2 files

---
+++ util/auth.py
@@ -0,0 +1,25 @@
+from request import Request
+
+#---extract_credentials Method---#
+#  -Parameters: A request object (from util/request.py)
+#  -Return: List of 2 elements (A username and a password both strs)
+#  -Objective:
+#---#
+def extract_credentials():
+    return
+
+
+
+#---validate_password Method---#
+#  -Parameters: A string representing a password
+#  -Return: A boolean 
+#  -Objective: Specifies true or false if the password meets all 6 criteria to be considered acceptable
+#       1. The length of the password is at least 8
+#       2. The password contains at least 1 lowercase letter
+#       3. The password contains at least 1 uppercase letter
+#       4. The password contains at least 1 number
+#       5. The password contains at least 1 of the 12 special characters ('!', '@', '#', '$', '%', '^', '&', '(', ')', '-', '_', '=')
+#       6. The password does not contain any invalid characters (eg. any character that is not an alphanumeric or one of the 12 special characters)
+#---#
+def validate_password():
+    return
---
+++ util/router.py
@@ -0,0 +1,26 @@
+from request import Request
+import re
+
+class Router:
+
+    #Constructor with no parameters
+    def __init__(self):
+        print("I'm an object with no parameters :)")
+
+    #---add_route Method---#
+    #  -Parameters: A The HTTP method (str), the path (str), and a function that takes a Request object (from util/request.py)
+    #  -Return: Byte array that will be the response to be sent to client
+    #  -Objective: Adds a route to a router
+    #---#
+    def add_route(self, http_method: str, path: str, request: Request):
+        return
+    
+    #---route_request Method---#
+    #  -Parameters: A request object (from util/request.py)
+    #  -Return: Byte array from function associated with given path
+    #  -Objective: Checks the method and path of the request, then determines which added path should be used
+    #   calls the function associated with that path, and then returns the bytes returned by that method.
+    #   If no path for request, return 404. If multiple paths, use route added first with the add_route method.
+    #---#
+    def route_request(self, request: Request):
+        return
