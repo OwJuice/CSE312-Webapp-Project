0b0808d4670752c44e60612c8d622619f2059a03
OwJuice <76753394+OwJuice@users.noreply.github.com>
2024-03-21 22:35:39 -0400

    Fully implemented router for hw1 stuff and cleaned up server.py

--- server.py
+++ server.py
@@ -8,23 +8,22 @@ from util.router import Router
 from util.requestHandler import *
 
 
-#Router:
+#Router with added routes:
 router = Router()
+router.add_route("GET", "/$", server_root)
+router.add_route("GET", "/public/functions.js$", server_js)
+router.add_route("GET", "/public/webrtc.js$", server_webrtc_js)
+router.add_route("GET", "/public/style.css$", server_css)
+router.add_route("GET", "/public/favicon.ico$", server_favicon)
+router.add_route("POST", "/chat-messages$", server_post_chat_msgs)
+router.add_route("GET", "/chat-messages$", server_get_chat_msgs)
+router.add_route("GET", "/chat-messages/", server_get_chat_msg)
+router.add_route("DELETE", "/chat-messages/", server_delete_chat_msg)
+router.add_route("PUT", "/chat-messages/", server_update_chat_msg)
+router.add_route("GET", "/public/image/", server_image)
+
 router.add_route("post", "/register$", server_register)
 router.add_route("post", "/login$", server_login)
-#router.add_route(method: "GET", path: "/$", server_root()) # If path is "/" then 404 never gets hit
-
-#----We can add all our routes:
-#router.add_route(method: "GET", path: "/$", server_root())
-#router.add_route(method: "GET", path: "/public/functions.js", server_js())
-#router.add_route(method: "GET", path: "/public/image/.", server_js()) #Use regex to save time instead of $ to hardcode every image
-#router.add_route(method: "GET", path: "/chat-message/.", server_js()) #With ID message so only do that message (DO THIS WITH REGEX)
-#router.add_route(method: "GET", path: "/chat-message/", server_js()) #No ID so serve every chat message
-
-#These parameter values should be stored in a data structure. Maybe Path class to store those values or a list. Then you can iterate
-#over that data structure to see if paths match request.
-
-#TODO: Need to move all functions into the router. We call sendall after calling route_request that helps build the response. Each function for paths should build response in requestHandler
 
 class MyTCPHandler(socketserver.BaseRequestHandler):
 
@@ -36,12 +35,6 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
         print("--- end of data ---\n\n")
         request = Request(received_data)
 
-        #GETTING RESPONSE
-        #response = router.route_request(request)
-        #self.request.sendall(response)
-
-        # TODO: Parse the HTTP request and use self.request.sendall(response) to send your response
-        # NOTE: .decode converts bytes to string, .encode converts string to bytes
         # NOTE: sendall method sends information back to the client
 
         req_body = request.body
@@ -51,203 +44,8 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
         req_headers = request.headers
         req_cookies = request.cookies
 
-
-        print("************THE MESSAGE_PATH IN THE BEGINNING IS: " + str(req_path))
-
-        #Root path: If path is "/", then respond with the public/index.html file
-        if (req_path == "/"):
-            readfile = fileReader("./public/index.html")
-            visits_counter = int(req_cookies.get("visits", 0))
-            visits_counter += 1 #Increment visits counter by 1
-            visits_str = str(visits_counter)
-            readfile = readfile.replace("{{visits}}", visits_str)
-            encoded_file = readfile.encode()
-            length_of_file = str(len(encoded_file))
-            self.request.sendall(("HTTP/1.1 200 OK\r\nX-Content-Type-Options: nosniff\r\nContent-Type: text/html; charset=utf-8\r\nContent-Length: " + length_of_file + "\r\nSet-Cookie: visits=" + visits_str + "; Max-Age=3600\r\n\r\n").encode() + encoded_file)
-            #self.request.sendall(buildResponse("200 OK", "text/plain; charset=utf-8", readfile))
-
-        #If path is to /public/functions.js, then serve that js code
-        elif (req_path == "/public/functions.js"):
-            readfile = fileReader("./public/functions.js")
-            self.request.sendall(buildResponse("200 OK", "text/javascript; charset=utf-8", readfile))
-
-        #If path is to /public/webrtc.js, then serve that js code
-        elif (req_path == "/public/webrtc.js"):
-            readfile = fileReader("./public/webrtc.js")
-            self.request.sendall(buildResponse("200 OK", "text/javascript; charset=utf-8", readfile))
-
-        #If path is to /public/style.css, then serve that css code
-        elif (req_path == "/public/style.css"):
-            readfile = fileReader("./public/style.css")
-            self.request.sendall(buildResponse("200 OK", "text/css; charset=utf-8", readfile))
-
-        #If path is to /public/favicon.ico, then serve that icon
-        elif (req_path == "/public/favicon.ico"):
-            readfile = imageReader("./public/favicon.ico")
-            self.request.sendall(buildImageResponse("200 OK", "image/x-icon", readfile))
-
-        #If path is to /chat-messages (POST request), then store the incoming message in the database
-        #The message sent, the username, and a unique id for the message is stored.
-        elif (req_path == "/chat-messages"):
-            if (req_method == "POST"):
-                #Escape HTML before inserting msg into database
-                chat_message = req_body.decode()
-                safe_message = htmlInjectionPreventer(chat_message)
-
-                message_document = dbHandler.insertChatMessage(safe_message)
-                self.request.sendall(buildResponse("201 Created", "application/json; charset=utf-8", message_document))
-                
-            if (req_method == "GET"):
-                chat_messages = dbHandler.getAllChatMessages() #chat_messages is a list of json objects
-                self.request.sendall(buildResponse("200 OK", "application/json; charset=utf-8", chat_messages))
-                # replace_string = ""
-                # for chat_message in chat_messages:
-                #     current_message = chat_message["message"] #Now curent_message contains the actual message
-                #     replace_string += "<p>" + current_message + "</p>\n"
-        
-        #If path is to /chat-messages/<id>, and method is GET request, the body of the response will be the JSON object containing the
-        # requested path.
-        #If method is DELETE request, then delete the document from the database, no body in the response.
-        #If method is PUT request, then the body of request will be JSON object message .
-        elif ("/chat-messages/" in req_path):
-            #print("--------------THE REQPATH IS: " + str(req_path))
-            stripped_path = req_path.strip("/")
-            message_path = stripped_path.split("/") #Message_path is a string at this point, including the id
-            #print("--------------THE MESSAGE_PATH IS: " + str(message_path))
-
-            #Try to convert the message to an int, if it doesn't work, then send a 404 error because the id wasn't int compatible
-            try:
-                message_id = int(message_path[1])
-                #print("--------------THE MESSAGE_ID IS: " + str(message_id))
-            except ValueError:
-                # Handle the case where message_id_str is not a valid integer
-                self.request.sendall(buildResponse("404 Not Found", "text/plain; charset=utf-8", "Message ID is not valid >:("))
-            else:
-                #Now we know that the message id is a valid int. Next, find out if the request is a GET or DELETE
-                if (req_method == "GET"):
-                    #print("--------------THE REQPATH TO GET IS: " + str(req_path))
-                    target_message = dbHandler.getOneChatMessage(message_id)
-                    if target_message != None:
-                        self.request.sendall(buildResponse("200 OK", "application/json; charset=utf-8", target_message))
-                    else:
-                        self.request.sendall(buildResponse("404 Not Found", "text/plain; charset=utf-8", "Message not found :("))
-                elif (req_method == "DELETE"):
-                    #print("************THE MESSAGE_ID TO DELETE IS: " + str(message_id))
-                    dbHandler.deleteChatMessage(message_id)
-                    self.request.sendall("HTTP/1.1 204 No Content\r\nContent-Length: 0\r\n\r\n".encode())
-                #If PUT request, then update the given ID from the request path's document with the username AND message (contained in body of request)
-                elif (req_method == "PUT"):
-                    #print("************THE MESSAGE_ID TO PUT IS: " + str(message_id))
-                    update_check = dbHandler.update_document(message_id, req_body)
-                    if update_check is True:
-                        updated_message = dbHandler.getOneChatMessage(message_id)
-                        self.request.sendall(buildResponse("200 OK", "application/json; charset=utf-8", updated_message))
-                    else:
-                        self.request.sendall(buildResponse("404 Not Found", "text/plain; charset=utf-8", "Message not found :("))
-                
-
-        # #If path is to an image path, read the image file and determine what image to send
-        # #Use a variable for the image name so we can serve multiple images
-    
-        # elif ("/public/image" in req_path):
-        #     image_path = req_path.split("/")
-        #     image_name = image_path[2]  #image_name contains the name of the image and the image type like "cat.jpg" or "elephant.jpg"
-        #     readfile = imageReader("public/image/" + image_name)
-        #     self.request.sendall(buildResponse("200 OK", "image/jpeg", readfile))
-
-        elif (req_path == "/public/image/cat.jpg"):
-             readfile = imageReader("./public/image/cat.jpg")
-             self.request.sendall(buildImageResponse("200 OK", "image/jpeg", readfile))
-
-        elif (req_path == "/public/image/dog.jpg"):
-             readfile = imageReader("./public/image/dog.jpg")
-             self.request.sendall(buildImageResponse("200 OK", "image/jpeg", readfile))
-
-        elif (req_path == "/public/image/eagle.jpg"):
-             readfile = imageReader("./public/image/eagle.jpg")
-             print("EAGLE IMAGES RECIEVED")
-             self.request.sendall(buildImageResponse("200 OK", "image/jpeg", readfile))
-        
-        elif (req_path == "/public/image/elephant.jpg"):
-             readfile = imageReader("./public/image/elephant.jpg")
-             self.request.sendall(buildImageResponse("200 OK", "image/jpeg", readfile))
-
-        elif (req_path == "/public/image/elephant-small.jpg"):
-             readfile = imageReader("./public/image/elephant-small.jpg")
-             self.request.sendall(buildImageResponse("200 OK", "image/jpeg", readfile))
-
-        elif (req_path == "/public/image/flamingo.jpg"):
-             readfile = imageReader("./public/image/flamingo.jpg")
-             self.request.sendall(buildImageResponse("200 OK", "image/jpeg", readfile))           
-
-        elif (req_path == "/public/image/kitten.jpg"):
-             readfile = imageReader("./public/image/kitten.jpg")
-             self.request.sendall(buildImageResponse("200 OK", "image/jpeg", readfile))     
-
-        #Else, if path is anything that shouldn't received content, reuturn a 404 response with msg saying content wasn't found. (plain text)
-        else:
-            self.request.sendall(buildResponse("404 Not Found", "text/plain; charset=utf-8", "The requested content does not exist :("))
-        
-
-
-
-#---buildResponse Function---#
-#   A helper function to build responses to be sent from the client to the server after receiving a request
-#   Takes a response code, a MIME type, and the content as strings
-#   Decodes those arguments, finds the length of the data, and puts together the whole response and returns as bytes
-#   Currently only works with 200 and 404 responses
-#---#
-def buildResponse(responseCode, mimeType, content) :
-    content_bytes = content.encode()
-    content_len = len(content_bytes)
-    response = "HTTP/1.1 " + responseCode + "\r\n"
-    response += "X-Content-Type-Options: nosniff\r\n"
-    response += "Content-Type: " + mimeType + "\r\n"    # Concatenate: "; charset=utf-8" after the mimeType for text
-    response += "Content-Length: " + str(content_len)
-    response += "\r\n\r\n"
-    response += content
-    encoded_response = response.encode()
-
-    return encoded_response
-
-#---buildImageResponse Function---#
-#   Same as buildResponse, except it doesn't encode the content because images are already in bytes
-#---#
-def buildImageResponse(responseCode, mimeType, content) :
-    content_len = len(content)
-    response = "HTTP/1.1 " + responseCode + "\r\n"
-    response += "X-Content-Type-Options: nosniff\r\n"
-    response += "Content-Type: " + mimeType + "\r\n"
-    response += "Content-Length: " + str(content_len)
-    response += "\r\n\r\n"
-    response = response.encode() + content
-
-    return response
-
-#---fileReader---#
-#   A helper function that just takes in a file as a string, opens it, reads it, and returns the entire file as a string
-def fileReader(filename):
-    f = open(filename, "r")
-    readfile = f.read()
-
-    return readfile
-
-#---imageReader---#
-#   A helper function that just takes in an image file as a string, opens it, reads it in bytes, and returns the file in a byte array
-def imageReader(filename):
-    f = open(filename, "rb")
-    readfile = f.read()
-
-    return readfile
-
-#---htmlInjectionPreventer---#
-#   A helper function that takes in a string and returns a string that escapes html characters
-def htmlInjectionPreventer(string):
-    safe_string = string.replace("&", "&ammp")
-    safe_string = safe_string.replace("<", "&lt")
-    safe_string = safe_string.replace(">", "&gt")
-    return safe_string
-        
+        response = router.route_request(request)
+        self.request.sendall(response)
 
 def main():
     host = "0.0.0.0"
--- util/requestHandler.py
+++ util/requestHandler.py
@@ -1,24 +1,188 @@
 from util.request import Request
-import util.dbHandler as dbHandler 
+import util.dbHandler as dbHandler
+import os
 
 #===requestHandler.py===#
 #  -This file holds functions that handle building responses to requests.
 
+#---buildResponse Function---#
+#   A helper function to build responses to be sent from the client to the server after receiving a request
+#   Takes a response code, a MIME type, and the content as strings
+#   Decodes those arguments, finds the length of the data, and puts together the whole response and returns as bytes
+#   Currently only works with 200 and 404 responses
+#   NOTE: .decode converts bytes to string, .encode converts string to bytes
+#---#
+def buildResponse(responseCode, mimeType, content) :
+    content_bytes = content.encode()
+    content_len = len(content_bytes)
+    response = "HTTP/1.1 " + responseCode + "\r\n"
+    response += "X-Content-Type-Options: nosniff\r\n"
+    response += "Content-Type: " + mimeType + "\r\n"    # Concatenate: "; charset=utf-8" after the mimeType for text
+    response += "Content-Length: " + str(content_len)
+    response += "\r\n\r\n"
+    response += content
+    encoded_response = response.encode()
 
+    return encoded_response
+
+#---buildImageResponse Function---#
+#   Same as buildResponse, except it doesn't encode the content because images are already in bytes
+#---#
+def buildImageResponse(responseCode, mimeType, content) :
+    content_len = len(content)
+    response = "HTTP/1.1 " + responseCode + "\r\n"
+    response += "X-Content-Type-Options: nosniff\r\n"
+    response += "Content-Type: " + mimeType + "\r\n"
+    response += "Content-Length: " + str(content_len)
+    response += "\r\n\r\n"
+    response = response.encode() + content
+
+    return response
+
+#---fileReader---#
+#   A helper function that just takes in a file as a string, opens it, reads it, and returns the entire file as a string
+def fileReader(filename):
+    f = open(filename, "r")
+    readfile = f.read()
+
+    return readfile
+
+#---imageReader---#
+#   A helper function that just takes in an image file as a string, opens it, reads it in bytes, and returns the file in a byte array
+def imageReader(filename):
+    f = open(filename, "rb")
+    readfile = f.read()
+
+    return readfile
+
+#---htmlInjectionPreventer---#
+#   A helper function that takes in a string and returns a string that escapes html characters
+def htmlInjectionPreventer(string):
+    safe_string = string.replace("&", "&ammp")
+    safe_string = safe_string.replace("<", "&lt")
+    safe_string = safe_string.replace(">", "&gt")
+    return safe_string
 
 def server_root(request:Request):
-    #ToDo: read index.html
-    return b"This is where response would go"
+    req_cookies = request.cookies
+
+    readfile = fileReader("./public/index.html")
+    visits_counter = int(req_cookies.get("visits", 0))
+    visits_counter += 1 #Increment visits counter by 1
+    visits_str = str(visits_counter)
+    readfile = readfile.replace("{{visits}}", visits_str)
+    encoded_file = readfile.encode()
+    length_of_file = str(len(encoded_file))
+    return ("HTTP/1.1 200 OK\r\nX-Content-Type-Options: nosniff\r\nContent-Type: text/html; charset=utf-8\r\nContent-Length: " + length_of_file + "\r\nSet-Cookie: visits=" + visits_str + "; Max-Age=3600\r\n\r\n").encode() + encoded_file
 
 #Some other function (Functions for endpoints of server)
 def server_js(request:Request):
-    #ToDo: read function.js
-    return b"This is where response would go"
+    readfile = fileReader("./public/functions.js")
+    return buildResponse("200 OK", "text/javascript; charset=utf-8", readfile)
+
+def server_webrtc_js(request:Request):
+    readfile = fileReader("./public/webrtc.js")
+    return buildResponse("200 OK", "text/javascript; charset=utf-8", readfile)
+
+def server_css(request:Request):
+    readfile = fileReader("./public/style.css")
+    return buildResponse("200 OK", "text/css; charset=utf-8", readfile)
+
+def server_favicon(request:Request):
+    readfile = imageReader("./public/favicon.ico")
+    return buildImageResponse("200 OK", "image/x-icon", readfile)
+
+def server_post_chat_msgs(request:Request):
+    req_body = request.body
+
+    #Escape HTML before inserting msg into database
+    chat_message = req_body.decode()
+    safe_message = htmlInjectionPreventer(chat_message)
+    message_document = dbHandler.insertChatMessage(safe_message)
+    return buildResponse("201 Created", "application/json; charset=utf-8", message_document)
+
+def server_get_chat_msgs(request:Request):
+    chat_messages = dbHandler.getAllChatMessages() #chat_messages is a list of json objects
+    return buildResponse("200 OK", "application/json; charset=utf-8", chat_messages)
+
+def server_get_chat_msg(request:Request):
+    req_path = request.path
+
+    stripped_path = req_path.strip("/")
+    message_path = stripped_path.split("/") #Message_path is a string at this point, including the id
+    #Try to convert the message to an int, if it doesn't work, then send a 404 error because the id wasn't int compatible
+    try:
+        message_id = int(message_path[1])
+        #print("--------------THE MESSAGE_ID IS: " + str(message_id))
+    except ValueError:
+        # Handle the case where message_id_str is not a valid integer
+        return buildResponse("404 Not Found", "text/plain; charset=utf-8", "Message ID is not valid >:(")
+    else:
+        target_message = dbHandler.getOneChatMessage(message_id)
+        if target_message != None:
+            return buildResponse("200 OK", "application/json; charset=utf-8", target_message)
+        else:
+            return buildResponse("404 Not Found", "text/plain; charset=utf-8", "Message not found :(")
+
+def server_delete_chat_msg(request:Request):
+    req_path = request.path
+
+    stripped_path = req_path.strip("/")
+    message_path = stripped_path.split("/") #Message_path is a string at this point, including the id
+    #Try to convert the message to an int, if it doesn't work, then send a 404 error because the id wasn't int compatible
+    try:
+        message_id = int(message_path[1])
+        #print("--------------THE MESSAGE_ID IS: " + str(message_id))
+    except ValueError:
+        # Handle the case where message_id_str is not a valid integer
+        return buildResponse("404 Not Found", "text/plain; charset=utf-8", "Message ID is not valid >:(")
+    else:
+        dbHandler.deleteChatMessage(message_id)
+        return "HTTP/1.1 204 No Content\r\nContent-Length: 0\r\n\r\n".encode()
+    
+def server_update_chat_msg(request:Request):
+    req_path = request.path
+    req_body = request.body
+
+    stripped_path = req_path.strip("/")
+    message_path = stripped_path.split("/") #Message_path is a string at this point, including the id
+    #Try to convert the message to an int, if it doesn't work, then send a 404 error because the id wasn't int compatible
+    try:
+        message_id = int(message_path[1])
+        #print("--------------THE MESSAGE_ID IS: " + str(message_id))
+    except ValueError:
+        # Handle the case where message_id_str is not a valid integer
+        return buildResponse("404 Not Found", "text/plain; charset=utf-8", "Message ID is not valid >:(")
+    else:
+        update_check = dbHandler.update_document(message_id, req_body)
+        if update_check is True:
+            updated_message = dbHandler.getOneChatMessage(message_id)
+            return buildResponse("200 OK", "application/json; charset=utf-8", updated_message)
+        else:
+            return buildResponse("404 Not Found", "text/plain; charset=utf-8", "Message not found :(")
 
 #Some other function (Functions for endpoints of server)
 def server_image(request:Request):
-    #ToDo: read filename from the path and read that file
-    return b"This is where response would go"
+    req_path = request.path
+
+    image_path = req_path.split("/")
+    #If path is: "/public/image/cat.jpg"
+    #image_path looks like: ['', 'public', 'image', 'cat.jpg']
+
+    #Check if image path is invalid path
+    if len(image_path) < 4 or image_path[3] == "":
+        return buildResponse("400 Bad Request", "text/plain", "Invalid image path")
+
+    image_name = image_path[3]  
+    #image_name contains the name of image & image type like "cat.jpg" or "elephant.jpg"
+    image_file_path = "public/image/" + image_name
+
+    #See if requested image exists
+    if not os.path.exists(image_file_path):
+        return buildResponse("404 Not Found", "text/plain", "Image not found")
+    
+    readfile = imageReader(image_file_path)
+    return buildImageResponse("200 OK", "image/jpeg", readfile)
 
 def server_register(request:Request):
     return
